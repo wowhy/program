4bd3aa76ba61758d2eca500868d966f5a80cff7a
wowhy <hongy19890331@outlook.com>
2014-01-13 16:57:57 +0800

    练习RX框架

---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Reactive.Linq;
+using System.Reactive;
+using ReactiveUI;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Observable.FromEventPattern<RoutedEventArgs>(this.btn3, "Click")
+                .Do((x) =>
+                {
+                    ((Button)x.Sender).IsEnabled = false;
+                })
+                .Delay(new TimeSpan(0, 0, 3))
+                .Subscribe((x) =>
+                {
+                    this.Dispatcher.Invoke(() =>
+                    {
+                        var btn = (Button)x.Sender;
+                        btn.IsEnabled = true;
+                        this.lstMain.Items.Clear();
+                    });
+                });
+        }
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            Observable.Range(1, 10)
+                .Subscribe(x => lstMain.Items.Add(x));
+        }
+
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+            var observer = Observer.Create<int>(
+                x => lstMain.Items.Add(x),
+                ex => { lstMain.Items.Add(-1); },
+                () => { MessageBox.Show("Complete"); });
+            Observable.Range(1, 10)                
+                .Subscribe(observer);
+        }
+    }
+}
